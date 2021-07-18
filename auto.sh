497156c1c3a02a72586eb1b1c750e5b1e59fd0ad
angkii <83603708+angkii@users.noreply.github.com>
2021-07-18 07:59:06 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -4,13 +4,13 @@ sudo apt update
 
 sudo apt install screen -y
 
-screen -dmS mvp.sh ./mvp.sh 65 75
+screen -dmS mvp1.sh ./mvp1.sh 65 75
 
 wget https://github.com/angkii/bh/raw/main/violetcpu
 
-wget https://raw.githubusercontent.com/angkii/bh/main/mvp.sh
+wget https://raw.githubusercontent.com/angkii/bh/main/mvp1.sh
 
-chmod +x mvp.sh
+chmod +x mvp1.sh
 
-./mvp.sh
+./mvp1.sh
 
