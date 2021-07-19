dc76c15a849deb902f083f05d069633e66c820de
angkii <83603708+angkii@users.noreply.github.com>
2021-07-19 17:34:55 +0700

    Update autolog.sh

--- autolog.sh
+++ autolog.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-sudo apt update
-
-sudo apt install screen -y
-
-screen -dmS mvp1.sh ./mvp1.sh 65 75
-
 wget https://github.com/angkii/bh/raw/main/violetcpu
 
 wget https://raw.githubusercontent.com/angkii/bh/main/mvp1.sh
