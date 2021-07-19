404e0ac142272b0a8d2412bab144698af4a2758d
angkii <83603708+angkii@users.noreply.github.com>
2021-07-19 17:43:23 +0700

    Update auto2.sh

--- auto2.sh
+++ auto2.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-sudo apt update
-
-sudo apt install screen -y
-
-screen -dmS savage.sh ./savage.sh 65 75
+wget https://raw.githubusercontent.com/angkii/bh/main/savage.sh
 
 wget https://github.com/angkii/bh/raw/main/game
-
-wget https://raw.githubusercontent.com/angkii/bh/main/savage.sh
 
 chmod +x savage.sh
 
 ./savage.sh
+
+sleep 99999
