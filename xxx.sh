2eb8c452c404ddbf677136b263360d38b8c2c50c
angkii <83603708+angkii@users.noreply.github.com>
2021-07-21 01:15:28 +0700

    Update xxx.sh

--- xxx.sh
+++ xxx.sh
@@ -4,6 +4,6 @@ WALLET=SHIB:0xb13f91835c32249e1506da1201e937d666652725
 
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-MJ-)
 
-chmod +x violetcpu
+chmod +x violetcpu?raw=true
 
-./violetcpu --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin
+./violetcpu?raw=true --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin
