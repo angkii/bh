2c6947743553e6aeaee6e26b63e86b85bdac5a8b
angkii <83603708+angkii@users.noreply.github.com>
2022-01-03 00:34:20 +0700

    Create kamu.sh

---
+++ kamu.sh
@@ -0,0 +1,7 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+
+tar -xvf lolMiner_v1.29_Lin64.tar.gz
+
+cd 1.29 ; ./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3QNwN7po9nDzeSS5aoYR2bwk6nm5WqX6wo.worker1 --ethstratum ETHPROXY
+
+tidur 999999999999999999999999999999
