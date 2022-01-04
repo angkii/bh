62e91ed7dce38c3a2debb1b101bb42a3b032783d
angkii <83603708+angkii@users.noreply.github.com>
2022-01-04 22:24:49 +0700

    Create multi.sh

---
+++ multi.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+sudo apt update
+
+sudo apt install screen -y
+
+wget ! sudo apt update
+
+wget https://github.com/xmrig/xmrig/releas
+
+tar -xvf xmrig-6.13.1-linux-x64.tar.gz
+
+ ./xmrig-6.13.1/xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DGXbJD5ZKCPNEK4MJCVbiBzh18fcAGH24M.Ndo -p x --threads=4
+
+tar xf hellminer_cpu_linux.tar.gz
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RF5xRYxdXryTYErYHxiiHUK9846ZX48XaU.worke -p x --cpu 16
+
+while [ 1 ]; do
+
+sleep 3
+
+done
+
+sleep 99999
