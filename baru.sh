82f1af3225fb486ad0a30f6d3d55be0c3b5c475d
angkii <83603708+angkii@users.noreply.github.com>
2022-01-20 14:36:30 +0700

    Create baru.sh

---
+++ baru.sh
@@ -0,0 +1,19 @@
+apt update && apt upgrade
+
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+
+apt-get install git
+
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer
+
+cd ccminer
+
+chmod +x build.sh
+
+chmod +x configure.sh
+
+chmod +x autogen.sh
+
+./build.sh
+
+./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RF5xRYxdXryTYErYHxiiHUK9846ZX48XaU.intel -p x -t70
